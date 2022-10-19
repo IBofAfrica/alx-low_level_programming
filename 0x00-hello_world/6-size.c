775af276c8baeec7561a58484a241e9990448f9b
Ibrahim Ukanah <ibofafrica@gmail.com>
2022-10-19 15:08:14 -0700

    finally

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	int d = sizeof(long long);
 	int e = sizeof(float);
 
-	printf("size of a char: %i bytes\n", a);
-	printf("size of an int: %i bytes\n", b);
-	printf("size of a long: %i bytes\n", c);
-	printf("size of a long long %i bytes\n", d);
-	printf("size of a float %i bytes\n", e);
+	printf("size of a char: %i byte(s)\n", a);
+	printf("size of an int: %i byte(s)\n", b);
+	printf("size of a long int: %i byte(s)\n", c);
+	printf("size of a long long int %i byte(s)\n", d);
+	printf("size of a float %i byte(s)\n", e);
 	return (0);
 }

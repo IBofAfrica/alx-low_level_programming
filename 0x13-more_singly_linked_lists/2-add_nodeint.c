40cc2eaeb88b4cc2e276a17dcb097d21d66eb2ee
IB Of Africa <ibofafrica@gmail.com>
2023-04-03 16:49:13 -0700

    test

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,12 +1,15 @@
 #include "lists.h"
 
 /**
- * add_nodeint - function that adds a new node at the beginning of
- * @head: header pointer
- * @n: int data type
- * Return: the new address
+ * add_nodeint - Adds a new node at the beginning
+ *               of a listint_t list.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
  */
-
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
@@ -14,8 +17,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->n = n;
 	new->next = *head;
+
 	*head = new;
+
 	return (new);
 }
